8a02a5a41e45c7f3501f374492755a7f0f575a1e
mohn_bgs <ssmemes163@gmail.com>
2025-05-19 00:21:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", ""))  # Replace "123456" with your actual api_id or use .env
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "20114039"))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Your Telegram user ID
-SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
+OWNER_ID = int(os.environ.get("OWNER_ID", "8172163893"))  # Your Telegram user ID
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "8172163893").split()))  # Space-separated user IDs
 
 MONGO_URL = os.environ.get("MONGO_URL", "")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-"))  # Telegram channel ID (with -100 prefix)
